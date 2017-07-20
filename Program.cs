c7efe17d3f41bd649004a4b774d9db04c5fac45f
Guilherme Jun Yoshimura <gjyoshimura@gmail.com>
2017-07-19 23:30:56 -0300

    google_calendar

---
+++ Program.cs
@@ -0,0 +1,120 @@
+﻿
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Calendar.v3.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CalendarQuickstart
+{
+    class Program
+    {
+        // If modifying these scopes, delete your previously saved credentials
+        // at ~/.credentials/calendar-dotnet-quickstart.json
+        static string[] Scopes = { CalendarService.Scope.Calendar };
+        static string ApplicationName = "Google Calendar API .NET Quickstart";
+
+        static void Main(string[] args)
+        {
+            UserCredential credential;
+
+            using (var stream =
+                new FileStream("client_secret.json", FileMode.Open, FileAccess.Read))
+            {
+                string credPath = System.Environment.GetFolderPath(
+                    System.Environment.SpecialFolder.Personal);
+                credPath = Path.Combine(credPath, ".credentials/calendar-dotnet-quickstart.json");
+
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
+
+            // Create Google Calendar API service.
+            var service = new CalendarService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+
+
+
+            // Insert New Event
+            Event newEvent = new Event()
+            {
+                Summary = "Google I/O 2017",
+                Location = "800 Howard St., San Francisco, CA 94103",
+                Description = "A chance to hear more about Google's developer products.",
+                Start = new EventDateTime()
+                {
+                    DateTime = DateTime.Parse("2017-07-20T09:00:00-07:00"),
+                    TimeZone = "America/Los_Angeles",
+                },
+                End = new EventDateTime()
+                {
+                    DateTime = DateTime.Parse("2017-07-20T17:00:00-07:00"),
+                    TimeZone = "America/Los_Angeles",
+                },
+                Recurrence = new String[] { "RRULE:FREQ=DAILY;COUNT=2" },
+                Attendees = new EventAttendee[] {
+                    new EventAttendee() { Email = "lpage@example.com" },
+                    new EventAttendee() { Email = "sbrin@example.com" },
+                },
+                Reminders = new Event.RemindersData()
+                {
+                    UseDefault = false,
+                    Overrides = new EventReminder[] {
+                        new EventReminder() { Method = "email", Minutes = 24 * 60 },
+                        new EventReminder() { Method = "sms", Minutes = 10 },
+                    }
+                }
+            };
+
+            String calendarId = "primary";
+            EventsResource.InsertRequest request1 = service.Events.Insert(newEvent, calendarId);
+            Event createdEvent = request1.Execute();
+            Console.WriteLine("Event created: {0}", createdEvent.HtmlLink);
+
+            // Define parameters of request.
+            EventsResource.ListRequest request = service.Events.List("primary");
+            request.TimeMin = DateTime.Now;
+            request.ShowDeleted = false;
+            request.SingleEvents = true;
+            request.MaxResults = 10;
+            request.OrderBy = EventsResource.ListRequest.OrderByEnum.StartTime;
+
+            // List events.
+            Events events = request.Execute();
+            Console.WriteLine("Upcoming events:");
+            if (events.Items != null && events.Items.Count > 0)
+            {
+                foreach (var eventItem in events.Items)
+                {
+                    string when = eventItem.Start.DateTime.ToString();
+                    if (String.IsNullOrEmpty(when))
+                    {
+                        when = eventItem.Start.Date;
+                    }
+                    Console.WriteLine("{0} ({1})", eventItem.Summary, when);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No upcoming events found.");
+            }
+            Console.Read();
+
+        }
+    }
+}
